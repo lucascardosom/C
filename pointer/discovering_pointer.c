72ec5cbc398a60bc56ec711dba2d1bd738cfa924
Lucas <lcmatos@id.uff.br>
2026-04-25 21:55:44 -0300

    Discovering how to use pointer

--- pointer/discovering_pointer.c
+++ pointer/discovering_pointer.c
@@ -3,10 +3,11 @@
 
 #include <stdio.h>
 
-void main(){
+int main(){
     int x = 10;
     int *p = &x;
     printf("the value of x is:%d",x);
     printf("the memory adress of x is:%d", &x);
     printf("the value from pointer is:%d", *p);
+return 0;
 }
